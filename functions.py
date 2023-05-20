0978b6fe6e02532c0321526b75ea22fb968f6922
TAMILVIP007 <indrajeethy.ut20@scew.org>
2023-05-20 16:28:31 +0530

    update

--- functions.py
+++ functions.py
@@ -2,6 +2,8 @@ import ibm_db
 from os import getenv
 from dotenv import load_dotenv
 import ibm_boto3
+import string
+import random
 from constants import *
 from ibm_botocore.client import Config
 
@@ -21,14 +23,15 @@ class Functions:
         try:
             ibm_db.exec_immediate(
                 self.conn, "CREATE TABLE IF NOT EXISTS registers (id INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY, name VARCHAR(50), email VARCHAR(50) NOT NULL UNIQUE, password VARCHAR(20), role VARCHAR(10));")
-            ibm_db.exec_immediate(self.conn, "CREATE TABLE IF NOT EXISTS complaints (id INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY, username VARCHAR(50), email VARCHAR(50) NOT NULL, image_url VARCHAR(100), title VARCHAR(100), description VARCHAR(100), progress VARCHAR(10), status VARCHAR(10), image_after_url VARCHAR(100), FOREIGN KEY (email) REFERENCES registers(email));")
+            ibm_db.exec_immediate(self.conn, "CREATE TABLE IF NOT EXISTS complaints (id INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY, username VARCHAR(50), email VARCHAR(50) NOT NULL, image_url VARCHAR(100), title VARCHAR(100), description VARCHAR(100), location_details VARCHAR(100), latitute VARCHAR(20), longitute VARCHAR(20), FOREIGN KEY (email) REFERENCES registers(email));")
+            ibm_db.exec_immediate(self.conn, "CREATE TABLE IF NOT EXISTS tasks (id INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY, username VARCHAR(50), email VARCHAR(50) NOT NULL, image_url VARCHAR(100), title VARCHAR(100), description VARCHAR(100), progress VARCHAR(10), status VARCHAR(10), image_after_url VARCHAR(100), agent_id VARCHAR(10), updated_status VARCHAR(100), FOREIGN KEY (email) REFERENCES registers(email));")
         except Exception as e:
             print(e)
         print("Connected to DB And  Created Tables")
 
     def check_exists(self, mail: str, password: str, type: str) -> bool:
         stmt = ibm_db.exec_immediate(
-            self.conn, f"SELECT * FROM registers WHERE EMAIL = '{mail}' AND PASSWORD = '{password}' AND TYPE = '{type}'")
+            self.conn, f"SELECT * FROM registers WHERE EMAIL = '{mail}' AND PASSWORD = '{password}' AND ROLE = '{type}'")
         return ibm_db.fetch_row(stmt)
 
     def check_user_exists(self, mail: str) -> bool:
@@ -36,23 +39,55 @@ class Functions:
             self.conn, f"SELECT * FROM registers WHERE EMAIL = '{mail}'")
         return ibm_db.fetch_row(stmt)
 
-    def add_newuser(self, name: str, mail: str, password: str, address: str, type: str) -> bool:
+    def add_newuser(self, name: str, mail: str, password: str, type: str) -> bool:
         try:
-            print(name, mail, password, address, type)
             ibm_db.exec_immediate(
-                self.conn, f"INSERT INTO registers (name, email, password, address, type) VALUES ('{name}', '{mail}', '{password}', '{address}', '{type}')")
+                self.conn, f"INSERT INTO registers (name, email, password, role) VALUES ('{name}', '{mail}', '{password}', '{type}')")
             return True
         except Exception as e:
             print(e)
             return False
 
-    def new_complaint(self, name: str, mail: str, subject: str, email: str) -> bool:
+    def new_complaint(self, title: str, description: str, mail: str, image_url: str, latitute: str, longitute: str, location_details: str) -> bool:
         try:
-            ibm_db.exec_immediate(
-                self.conn, f"INSERT INTO complaints (name, email, subject) VALUES ('{name}', '{email}', '{subject}', '{mail}', '{user_id}')")
+            query = "INSERT INTO complaints (username, email, image_url, title, description, location_details, LATITUTE, LONGITUTE) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')"
+            query = query.format(mail.split('@')[0], mail, image_url, title, description,location_details , latitute, longitute)
+
+            ibm_db.exec_immediate(self.conn, query)
             return True
-        except:
+        except Exception as e:
+            print(e)
             return False
+        
+
+    def get_complaint(self):
+        try:
+            query = "SELECT * FROM complaints"
+            stmt = ibm_db.exec_immediate(self.conn, query)
+            result = ibm_db.fetch_assoc(stmt)
+    
+
+            complaints = []
+            while result:
+                complaint = {
+                    'id': result['ID'],
+                    'username': result['USERNAME'],
+                    'email': result['EMAIL'],
+                    'image_url': result['IMAGE_URL'],
+                    'title': result['TITLE'],
+                    'description': result['DESCRIPTION'],
+                    'location_details': result['LOCATION_DETAILS'],
+                    'latitude': result['LATITUTE'],
+                    'longitude': result['LONGITUTE']
+                }
+                complaints.append(complaint)
+                result = ibm_db.fetch_assoc(stmt)
+           
+            return complaints
+        except Exception as e:
+            print(e,"is the error")
+            return []
+
 
     def get_complaints(self) -> list:
         stmt = ibm_db.exec_immediate(self.conn, "SELECT * FROM complaints")
@@ -70,14 +105,18 @@ class Functions:
             print(e)
             return None
 
-    def upload_file(self, path: str) -> bool:
+    def upload_file(self, path: str) -> str:
         try:
             self.cos.upload_file(
                 Filename=path, Bucket=COS_BUCKET_NAME, Key=path)
-            return True
+            return f"{COS_ENDPOINT}{COS_BUCKET_NAME}/{path}"
         except Exception as e:
             print(e)
-            return False
+            return None
+
+    def generate_random_string(self):
+        letters = string.ascii_lowercase
+        return ''.join(random.choice(letters) for i in range(10)) + '.jpg'
 
 
 func = Functions()
--- main.py
+++ main.py
@@ -6,103 +6,141 @@ app = Flask(__name__)
 app.secret_key = 'lmao'
 
 
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route("/admin/new_user", methods=['POST'])
-def regpt():
+@app.route('/<role>/home')
+def homeroute(role):
+    if 'user' in session:
+        if role == 'admin':
+            username = 'John Doe'
+            complaints = [
+                {
+                    'id': 1,
+                    'username': 'John Doe',
+                    'image_url': 'path/to/image.jpg',
+                    'title': 'Issue Title',
+                    'description': 'Issue Description',
+                    'progress': '50%',
+                    'status': 'In Progress',
+                    'image_after_url': 'path/to/image.jpg'
+                },
+                {
+                    'id': 2,
+                    'username': 'Jane Smith',
+                    'image_url': 'path/to/image.jpg',
+                    'title': 'Issue Title',
+                    'description': 'Issue Description',
+                    'progress': '100%',
+                    'status': 'Completed',
+                    'image_after_url': 'path/to/image.jpg'
+                }
+                # Add more complaints as needed
+            ]
+            partners = [
+                {'id': 1, 'name': 'Partner A'},
+                {'id': 2, 'name': 'Partner B'},
+                {'id': 3, 'name': 'Partner C'}
+                # Add more partners as needed
+            ]
+            return render_template('adminhome.html', username=username, complaints=complaints, partners=partners)
+        return render_template(role+'home.html', username=session['user'])
+    else:
+        return redirect(url_for('loginpage', role=role))
+
+
+@app.route("/<role>/new_user", methods=['POST'])
+def regpt(role):
     name = request.form['rname']
     email = request.form['remail']
     password = request.form['rpassword']
     admincode = request.form['admincode']
     print(name, email, password, admincode)
-    if admincode not in AUTH_ADMIN_CODES:
+    if role == 'admin' and admincode not in AUTH_ADMIN_CODES:
         return render_template('admin.html', error="Invalid Admin Code")
     if func.check_user_exists(email):
-        return "User already exists"
-    ok = func.add_newuser(name, email, password, 'admin')
-    if ok:
+        return render_template('admin.html', error="User already exists")
+    if func.add_newuser(name, email, password, role):
         session['user'] = email
-        return redirect(url_for('regpt', error="User Created"))
+        return redirect(url_for('loginpage', role=role))
     else:
-        return "Error"
+        return render_template('admin.html', error="Error creating user")
+
+
+
 
 
 @app.route("/<role>/login")
 def loginpage(role):
     return render_template(role+'.html', role=role)
 
+@app.route("/<role>/login", methods=['POST'])
+def loginpost(role):
+    email = request.form['email']
+    password = request.form['password']
+    if func.check_exists(email, password, role):
+        session['user'] = email
+        return redirect(url_for('homeroute', role=role))
+    else:
+        return render_template(role+'.html', error="Invalid Credentials")
+
 
 @app.route('/adminhome')
 def adminhome():
     username = 'John Doe'
-    complaints = [
-        {
-            'id': 1,
-            'username': 'John Doe',
-            'image_url': 'path/to/image.jpg',
-            'title': 'Issue Title',
-            'description': 'Issue Description',
-            'progress': '50%',
-            'status': 'In Progress',
-            'image_after_url': 'path/to/image.jpg'
-        },
-        {
-            'id': 2,
-            'username': 'Jane Smith',
-            'image_url': 'path/to/image.jpg',
-            'title': 'Issue Title',
-            'description': 'Issue Description',
-            'progress': '100%',
-            'status': 'Completed',
-            'image_after_url': 'path/to/image.jpg'
-        }
-        # Add more complaints as needed
-    ]
+    
+    # Retrieve complaint details from the database
+    complaints = func.get_complaint()  # Assuming you have implemented the get_complaints() function
+    
     partners = [
         {'id': 1, 'name': 'Partner A'},
         {'id': 2, 'name': 'Partner B'},
         {'id': 3, 'name': 'Partner C'}
         # Add more partners as needed
     ]
+    
     return render_template('adminhome.html', username=username, complaints=complaints, partners=partners)
+
 
 
 @app.route('/agenthome')
 def agenthome():
     agent_name = 'John Doe'
     agent_id = 1234
-    complaints = [
-        {
-            'id': 1,
-            'image_url': 'https://example.com/image1.jpg',
-            'location_details': '123 Main St.',
-            'file_id': 'abc123',
-            'title': 'Broken Street Light',
-            'description': 'The street light on Main St. is broken and needs to be fixed.',
-            'assigned_partner': 1234,
-            'progress': 'inprogress',
-            'updated_status': 'In Progress',
-            'image_after_url': ''
-        },
-          {
-            'id': 1,
-            'image_url': 'https://example.com/image1.jpg',
-            'location_details': '123 Main St.',
-            'file_id': 'abc123',
-            'title': 'Broken Street Light',
-            'description': 'The street light on Main St. is broken and needs to be fixed.',
-            'assigned_partner': 1234,
-            'progress': 'inprogress',
-            'updated_status': 'In Progress',
-            'image_after_url': ''
-        }
-    ]
+    complaints = func.get_complaint()
+   
     return render_template('agenthome.html', agent_name=agent_name, agent_id=agent_id, complaints=complaints)
+
+
+@app.route('/userhome')
+def usrhome():
+    flash("a")
+    return render_template('userhome.html', username=session.get('user'))
+
+
+
+@app.route('/submit-complaint', methods=['POST'])
+def submit_complaint():
+    file = request.files['complaint_image']
+    title = request.form['complaint_type']
+    description = request.form['complaint_description']
+    latitute = request.form['latitude']
+    longitute = request.form['longitude']
+    location_details = request.form['location_details']
+    mail = session.get('user')
+    print(title, description, latitute, longitute, location_details, mail)
+    if file.filename == '':
+        return redirect("/userhome")
+    if file:
+        filename = f"temp/{func.generate_random_string()}"
+        file.save(filename)
+        image_url = func.upload_file(filename)
+        func.new_complaint(title, description, mail, image_url, latitute, longitute, location_details)
+        flash(message="Complaint submitted successfully", category="success")
+        return redirect("/userhome")
+    return render_template('userhome.html')
 
 
 if __name__ == '__main__':
